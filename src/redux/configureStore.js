f81028da56bcbe4cb9fd3d2c8d0a1678b8c89898
Abdulfatai Badara <fobadara@gmail.com>
2022-03-07 12:18:10 +0100

    Configure the store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import usersReducer from './usersReducer';
-// import postsReducer from './postsReducer';
+import { configureStore } from '@reduxjs/toolkit';
+import historyReducer from './currencies/currencies';
+import countryReducer from './countries/countries';
 
-// const store = configureStore({
-//   reducer: {
-//     users: usersReducer,
-//     posts: postsReducer,
-//   },
-// });
+const reducer = {
+  history: historyReducer,
+  country: countryReducer,
+};
 
-// export default store;
+const store = configureStore({
+  reducer,
+});
+
+export default store;
